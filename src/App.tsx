232ea26007d70ff6e7bf5c3aa1edd9f1c2eed047
1446450047 <1446450047@qq.com>
2020-06-16 09:47:03 +0800

    导航栏布局搞定

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,94 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    border:1px solid red;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
+
+const Content = styled.div`
+border: 1px solid green;
+flex-grow: 1;
+`;
+const Nav = styled.div`
+      &>ul{
+         display: flex;   
+         justify-content: center;   
+         align-items: center;
+         text-align: center;
+         >li{
+         padding: 1em;
+            width: 33.3333%;
+         }
+      }
+border: 1px solid yellow;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Wrapper>
+                <Content>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags/>
+                        </Route>
+                        <Route path="/money">
+                            <Money/>
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics/>
+                        </Route>
+                        <Redirect exact from="/" to="money"/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Content>
+                <Nav>
+                    <ul>
+                        <li>
+                            <Link to="/tags">tags</Link>
+                        </li>
+                        <li>
+                            <Link to="/money">money</Link>
+                        </li>
+                        <li>
+                            <Link to="/statistics">statistics</Link>
+                        </li>
+                    </ul>
+                </Nav>
+            </Wrapper>
+        </Router>
+    );
+}
+
+function NoMatch() {
+    return (
+        <div>你输入的页面不存在，请输入正确的页面</div>
+    )
+}
+
+function Statistics() {
+    return <h2>statistics</h2>;
+}
+
+function Tags() {
+    return <h2>tags</h2>;
+}
+
+function Money() {
+    return <h2>money</h2>;
 }
 
 export default App;
