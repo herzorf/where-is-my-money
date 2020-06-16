2a1eb254f2dd992da4ae29c51ea25d6b1150fbbe
1446450047 <1446450047@qq.com>
2020-06-16 15:13:46 +0800

    添加了svg图片和配色

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
+import x from "icons/tag.svg"
 
 const NavWrapper = styled.div`
       &>ul{
@@ -8,9 +9,10 @@ const NavWrapper = styled.div`
          justify-content: center;   
          align-items: center;
          text-align: center;
-         >li{
-         padding: 1em;
+          >li{
+            padding: 1em;
             width: 33.3333%;
+            color:#1296db;
          }
       }
       box-shadow: 0 0 3px rgb(0,0,0,0.25);
@@ -20,6 +22,7 @@ function Nav(){
         <NavWrapper>
             <ul>
                 <li>
+                    <img src={x} alt=""/>
                     <Link to="/tags">标签</Link>
                 </li>
                 <li>
