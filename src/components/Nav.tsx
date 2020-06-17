119deaf2287ea4acf5302427f84912efe19f7ed0
1446450047 <1446450047@qq.com>
2020-06-17 17:05:44 +0800

    修复Icon Nav 点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,14 +9,20 @@ const NavWrapper = styled.div`
          align-items: center;
          text-align: center;
           >li{
-            padding: 0.4em;
+            border: 1px solid red;
             width: 33.3333%;
             color:#1296db;
+            padding: 0.4em;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          >a{
+           border: 1px solid yellow;
             display: flex;
             flex-direction: column;
             justify-content: center;
-            align-items: center;
-         }
+            align-items: center;          
+          }}
       }
       box-shadow: 0 0 3px rgb(0,0,0,0.25);
 `;
@@ -25,16 +31,19 @@ function Nav(){
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name="tag"/>
-                    <Link to="/tags">标签</Link>
+                    <Link to="/tags">
+                        <Icon name="tag"/>
+                        标签</Link>
                 </li>
                 <li>
-                    <Icon name="add"/>
-                    <Link to="/money">记账</Link>
+                    <Link to="/money">
+                        <Icon name="add"/>
+                        记账</Link>
                 </li>
                 <li>
-                    <Icon name="statistics"/>
-                    <Link to="/statistics">统计</Link>
+                    <Link to="/statistics">
+                        <Icon name="statistics"/>
+                        统计</Link>
                 </li>
             </ul>
         </NavWrapper>
