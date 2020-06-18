7a40a8bac6e5889a394dab338862b716278e9233
1446450047 <1446450047@qq.com>
2020-06-18 09:46:23 +0800

    完成了NoteWrapper的样式

--- src/views/Add.tsx
+++ src/views/Add.tsx
@@ -25,7 +25,28 @@ const TagsSection = styled.section`
 `;
 
 const NotesSection = styled.section`
-
+    background: #17706e;
+    color: #f7f7ee;
+    display: flex;
+    padding: 10px 18px;
+    display: block;
+    >label{
+        display: flex;
+        align-items: center;
+    >span{
+      white-space: nowrap;
+      margin-right: 12px;
+    }
+    >input{
+        display: block;
+        width: 100%;height: 3em;
+        background: none;
+        border: none;
+        color: #f7f7ee;
+        font-size: 16px;
+        
+    }
+    }
 `;
 
 const TabsSection = styled.section`
@@ -54,7 +75,7 @@ function Add() {
                 <NotesSection>
                     <label>
                         <span>备注</span>
-                        <input type="text"/>
+                        <input type="text" placeholder="点击此处添加备注"/>
                     </label>
                 </NotesSection>
                 <TabsSection>
