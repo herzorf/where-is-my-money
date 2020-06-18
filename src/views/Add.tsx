c8d5e9af932d7de21075283c2d714aa5139e7256
1446450047 <1446450047@qq.com>
2020-06-18 17:37:17 +0800

    数子面板的UI搞定

--- src/views/Add.tsx
+++ src/views/Add.tsx
@@ -85,7 +85,50 @@ const TabsSection = styled.section`
 `;
 
 const NumberSection = styled.section`
-
+      display: flex;
+      flex-direction: column;
+      >.output{
+        font-size: 40px;
+        padding: 4px 12px;
+        text-align: right;
+        box-shadow:inset 0 -5px 5px -5px #17706e;
+      }
+      >.numbers{
+        >button{
+        font-size: 20px;
+        border: none;
+        background-color: #17706e;
+        color: #f7f7ee;
+        float: left;
+        border-radius: 8px;
+        box-shadow: inset 1px 1px 1px #f7f7ee;          
+        width: 25%;
+          height: 64px;
+          &:nth-child(4){
+            background-color: #fb7813;
+          }
+          
+          &:nth-child(8){
+            background-color: #f7f7ee;
+            border: 2px solid #17706e;
+            color: #17706e;
+            &:active{
+              background-color: #17706e;
+              color: #f7f7ee;
+              border: none;
+            }
+          }
+          &.OK{
+            height: 128px;
+            float: right;
+            background-color: #1296db;
+          }
+       &:active{
+         background-color: #f7f7ee;
+         color:#17706e;
+       }
+      }
+     }
 `;
 
 
@@ -116,8 +159,8 @@ function Add() {
                     </ul>
                 </TabsSection>
                 <NumberSection>
-                    <div>100</div>
-                    <div>
+                    <div className="output">100</div>
+                    <div className="numbers">
                         <button>1</button>
                         <button>2</button>
                         <button>3</button>
@@ -129,7 +172,7 @@ function Add() {
                         <button>7</button>
                         <button>8</button>
                         <button>9</button>
-                        <button>确定</button>
+                        <button className="OK">确定</button>
                         <button>0</button>
                         <button>.</button>
                         <button>%</button>
